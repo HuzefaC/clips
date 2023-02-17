506e0803402d012140408725fdff2f39c730ef32
huzefa <huzefachabukswar@gmail.com>
2023-02-17 10:28:33 +0530

    feat: Add feature to open auth modal on Login/Register click and close the modal on close click

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalService {
+  private visible: boolean = false;
 
   constructor() { }
+
+  isModalOpen(): boolean {
+    return this.visible;
+  }
+
+  toggleModal(): void {
+    this.visible = !this.visible;
+  }
 }
