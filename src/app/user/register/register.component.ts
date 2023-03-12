f02ee0274faa7b0337396e72be3a04554e8ab28d
huzefa <huzefachabukswar@gmail.com>
2023-03-13 02:45:37 +0530

    feat: Create a reusable input component with errors and validations.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,19 +1,32 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(50),
+  ]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(15),
+    Validators.max(100),
+  ]);
+  password = new FormControl('');
+  confirmPassword = new FormControl('');
+  phoneNumber = new FormControl('');
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
   });
-
 }
