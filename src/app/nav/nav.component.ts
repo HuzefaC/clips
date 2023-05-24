5a656347ebb9bf1f8bb702ca772842bb8bcc13d2
huzefa <huzefachabukswar@gmail.com>
2023-05-24 10:43:24 +0530

    feat: Add the logout feature

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit {
-  constructor(public modalService: ModalService, public auth: AuthService) {}
-
-  ngOnInit(): void {}
+export class NavComponent {
+  constructor(
+    public modalService: ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
 
   openModal($event: Event) {
     $event.preventDefault();
     this.modalService.toggleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+    await this.afAuth.signOut();
   }
 }
