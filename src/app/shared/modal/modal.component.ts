45c7bc90e857c3f53cc7c21ab3fda1514fb35b89
huzefa <huzefachabukswar@gmail.com>
2023-05-24 10:22:06 +0530

    feat: Remove the modal from dom when on component destroy

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,23 +1,24 @@
-import {Component, Input, OnInit, ElementRef} from '@angular/core';
-import {ModalService} from '../../services/modal.service';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
-
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalID: string = '';
 
-  constructor(public modalService: ModalService, public el: ElementRef) {
+  constructor(public modalService: ModalService, public el: ElementRef) {}
 
+  ngOnDestroy(): void {
+    document.body.removeChild(this.el.nativeElement);
   }
 
   ngOnInit(): void {
     // This will solve the issue of modal inheriting CSS from parent elements.
     // Following code will make the modal a child of the body
-    document.body.appendChild(this.el.nativeElement)
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal() {
